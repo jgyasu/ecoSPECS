df2c216043e54f7535a3112ec505890a69945821
Jigyasu <jigyasu@outlook.in>
2025-04-16 16:07:34 +0530

    main.py draft

---
+++ main.py
@@ -0,0 +1,6 @@
+from src.generation import generate_response
+from src.generation import gen_few_shot_prompt
+
+from src.parsing import extract_tables_from_docx
+from src.parsing import extract_tables_from_pdf
+
---
+++ src/generation/__init__.py
@@ -0,0 +1,5 @@
+from .gen_few_shot_prompt import gen_few_shot_prompt
+from .gen_model import gen_response
+
+__all__ = [gen_few_shot_prompt,
+           gen_model]
